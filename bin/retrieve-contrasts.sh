5848330b0409b7fef4ad127785e8e255d7e2248f
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-11-06 15:18:37 +0000

    Add modules for archive, enrichment analysis, download, contrasts and expressions

---
+++ bin/retrieve-contrasts.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+# 2. Retrieve contrastdetails.tsv and assaygroupsdetails.tsv from wwwdev.gxa and put them into $ATLAS_EXPS
+# This step doesn't seem to require step 1, and could be run separately/parallely.
+stagingApiUrl=${stagingApiUrl:-https://wwwdev.ebi.ac.uk/gxa/api}
+
+[ ! -z ${ATLAS_EXPS+x} ] || ( echo "Env var ATLAS_EXPS path to the experiment needs to be defined." && exit 1 )
+[ ! -z ${ATLAS_FTP+x} ] || ( echo "Env var ATLAS_FTP path to the experiment needs to be defined." && exit 1 )
+
+contrastdetails=$ATLAS_EXPS/contrastdetails.tsv
+curl -s -o $contrastdetails "$stagingApiUrl/$f"
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to retrieve $stagingApiUrl/$f"
+    rm -rf $contrastdetails
+    exit 1
+fi
+assaygroupsdetails=$ATLAS_EXPS/assaygroupsdetails.tsv
+curl -s -o $assaygroupsdetails "$stagingApiUrl/$f"
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to retrieve $stagingApiUrl/$f"
+    rm -rf $assaygroupsdetails
+    exit 1
+fi
+
+## copy files to $ATLAS_FTP from $ATLAS_EXPS
+cp -p $contrastdetails $ATLAS_FTP/experiments
+cp -p $assaygroupsdetails $ATLAS_FTP/experiments
---
+++ bin/retrieve-expressions.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+[ ! -z ${dbConnection+x} ] || ( echo "Env var dbConnection needs to be defined." && exit 1 )
+[ ! -z ${ATLAS_EXPS+x} ] || ( echo "Env var ATLAS_EXPS path to the experiment needs to be defined." && exit 1 )
+
+echo "refresh materialized view VW_DIFFANALYTICS_DUMP WITH DATA;" | psql $dbConnection
+echo -e "Gene ID\tGene Name\tOrganism\tExperiment\tComparison id\tp-value\tlog2foldchange\tt-statistic" > $ATLAS_EXPS/differential_analytics.tsv
+echo "select IDENTIFIER, NAME, ORGANISM, EXPERIMENT, CONTRASTID, PVAL, LOG2FOLD, TSTAT from VW_DIFFANALYTICS_DUMP" \
+  | psql $dbConnection | tail -n +3 | perl -p -e 's|^ +||g' \
+  | perl -p -e 's/ +\| +/\t/g' | grep -v '^$' >> $ATLAS_EXPS/differential_analytics.tsv
+# Remove the last line (e.g. 5147277 rows selected.)
+sed -i '$ d'  $ATLAS_EXPS/differential_analytics.tsv
